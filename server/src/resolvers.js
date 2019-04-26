ba8841bd42d9da9fdf22d612e35ef41caa372798
ken <kenchen@pneumasoftware.com>
2019-04-26 11:17:45 +0800

    Part.3

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,8 +1,15 @@
-const { paginateResults } = require("./utils");
+const {
+  paginateResults
+} = require("./utils");
 
 module.exports = {
   Query: {
-    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+    launches: async (_, {
+      pageSize = 20,
+      after
+    }, {
+      dataSources
+    }) => {
       const allLaunches = await dataSources.launchAPI.getAllLaunches();
       // we want these in reverse chronological order
       allLaunches.reverse();
@@ -18,31 +25,109 @@ module.exports = {
         cursor: launches.length ? launches[launches.length - 1].cursor : null,
         // if the cursor of the end of the paginated results is the same as the
         // last item in _all_ results, then there are no more results after this
-        hasMore: launches.length
-          ? launches[launches.length - 1].cursor !==
-            allLaunches[allLaunches.length - 1].cursor
-          : false
+        hasMore: launches.length ?
+          launches[launches.length - 1].cursor !==
+          allLaunches[allLaunches.length - 1].cursor :
+          false
       };
     },
-    launch: (_, { id }, { dataSources }) =>
-      dataSources.launchAPI.getLaunchById({ launchId: id }),
-    me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+    launch: (_, {
+        id
+      }, {
+        dataSources
+      }) =>
+      dataSources.launchAPI.getLaunchById({
+        launchId: id
+      }),
+    me: (_, __, {
+      dataSources
+    }) => dataSources.userAPI.findOrCreateUser()
+  },
+
+  Mutation: {
+    login: async (_, {
+      email
+    }, {
+      dataSources
+    }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({
+        email
+      });
+      if (user) return Buffer.from(email).toString("base64");
+    },
+    bookTrips: async (_, {
+      launchIds
+    }, {
+      dataSources
+    }) => {
+      const results = await dataSources.userAPI.bookTrips({
+        launchIds
+      });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds
+      });
+
+      return {
+        success: results && results.length === launchIds.length,
+        message: results.length === launchIds.length ?
+          "trips booked successfully" :
+          `the following launches couldn't be booked: ${launchIds.filter(
+                id => !results.includes(id)
+              )}`,
+        launches
+      };
+    },
+    cancelTrip: async (_, {
+      launchId
+    }, {
+      dataSources
+    }) => {
+      const result = await dataSources.userAPI.cancelTrip({
+        launchId
+      });
+
+      if (!result)
+        return {
+          success: false,
+          message: "failed to cancel trip"
+        };
+
+      const launch = await dataSources.launchAPI.getLaunchById({
+        launchId
+      });
+      return {
+        success: true,
+        message: "trip cancelled",
+        launches: [launch]
+      };
+    }
   },
 
   Mission: {
     // make sure the default size is 'large' in case user doesn't specify
-    missionPatch: (mission, { size } = { size: "LARGE" }) => {
-      return size === "SMALL"
-        ? mission.missionPatchSmall
-        : mission.missionPatchLarge;
+    missionPatch: (mission, {
+      size
+    } = {
+      size: "LARGE"
+    }) => {
+      return size === "SMALL" ?
+        mission.missionPatchSmall :
+        mission.missionPatchLarge;
     }
   },
   Launch: {
-    isBooked: async (launch, _, { dataSources }) =>
-      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id })
+    isBooked: async (launch, _, {
+        dataSources
+      }) =>
+      dataSources.userAPI.isBookedOnLaunch({
+        launchId: launch.id
+      })
   },
+
   User: {
-    trips: async (_, __, { dataSources }) => {
+    trips: async (_, __, {
+      dataSources
+    }) => {
       // get ids of launches by user
       const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
 
@@ -56,43 +141,4 @@ module.exports = {
       );
     }
   },
-  Mutation: {
-    login: async (_, { email }, { dataSources }) => {
-      const user = await dataSources.userAPI.findOrCreateUser({ email });
-      if (user) return Buffer.from(email).toString("base64");
-    },
-    bookTrips: async (_, { launchIds }, { dataSources }) => {
-      const results = await dataSources.userAPI.bookTrips({ launchIds });
-      const launches = await dataSources.launchAPI.getLaunchesByIds({
-        launchIds
-      });
-
-      return {
-        success: results && results.length === launchIds.length,
-        message:
-          results.length === launchIds.length
-            ? "trips booked successfully"
-            : `the following launches couldn't be booked: ${launchIds.filter(
-                id => !results.includes(id)
-              )}`,
-        launches
-      };
-    },
-    cancelTrip: async (_, { launchId }, { dataSources }) => {
-      const result = await dataSources.userAPI.cancelTrip({ launchId });
-
-      if (!result)
-        return {
-          success: false,
-          message: "failed to cancel trip"
-        };
-
-      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
-      return {
-        success: true,
-        message: "trip cancelled",
-        launches: [launch]
-      };
-    }
-  }
 };
