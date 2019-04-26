d467e4102fb80060ba77aebc015be62e02d7b2ee
ken <kenchen@pneumasoftware.com>
2019-04-26 14:32:52 +0800

    Running test

--- client/src/pages/launch.js
+++ client/src/pages/launch.js
@@ -2,28 +2,22 @@ import React, { Fragment } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-import Loading from "../components/loading";
-import Header from "../components/header";
-import ActionButton from "../containers/action-button";
-import LaunchDetail from "../components/launch-detail";
+import { LAUNCH_TILE_DATA } from "./launches";
+import { Loading, Header, LaunchDetail } from "../components";
+import { ActionButton } from "../containers";
 
 export const GET_LAUNCH_DETAILS = gql`
   query LaunchDetails($launchId: ID!) {
     launch(id: $launchId) {
-      id
+      isInCart @client
       site
-      isBooked
       rocket {
-        id
-        name
         type
       }
-      mission {
-        name
-        missionPatch
-      }
+      ...LaunchTile
     }
   }
+  ${LAUNCH_TILE_DATA}
 `;
 
 export default function Launch({ launchId }) {
